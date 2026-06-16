ab1f6041d51b981a88f58e9dd9b2383147e145be
Mohamed AbdulkadirAbdulahi <maxamedgoley@gmail.com>
2026-06-17 00:07:41 +0300

    add the folders tinny fix things

---
+++ app/favorites/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <p>Favorite</p>
+    </div>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,20 @@ import { CiSearch } from "react-icons/ci";
 
 export default function Home() {
  return(
-  <div>
+  <div className="bg-[#0F0F0F] text-white min-h-screen">
   <div className=" bg-[#49414118] p-5 border-b border-gray-500">
    <div className="flex lg:justify-around justify-between items-center">
     <h1 className="flex flex-row gap-1"><span className="bg-red-600 px-1.5  font-bold text-xl rounded-lg">M</span><span className="text-xl font-bold">MovieFlix</span></h1>
     <div className="flex flex-row items-center gap-2 font-semibold hover:bg-[#67646418] cursor-pointer p-2 rounded-lg">
 <CiHeart size={25}/>
-<p>Favoites</p>
+<p>Favorites</p>
     </div>
    </div>
    </div>
 
-<div className="flex flex-col gap-2 p-5 mt-50  text-center">
+<div className="flex flex-col gap-2 p-5 mt-52  text-center">
   <h1 className="text-6xl font-bold">Discover Your Next Favorite Movie</h1>
-  <p className="text-gray-600 text-xl">Search, explore, and save the best movies from around the world.</p>
+  <p className="text-gray-400 text-xl">Search, explore, and save the best movies from around the world.</p>
 
 
 <div className="flex lg:w-[50%] w-[90%] flex-row gap-3 items-center mx-auto">
