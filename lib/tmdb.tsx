2ac8f3350c2297d01fbef1793a9634eda7f4407a
Mohamed AbdulkadirAbdulahi <maxamedgoley@gmail.com>
2026-06-21 11:53:28 +0300

    fix cache

--- lib/tmdb.tsx
+++ lib/tmdb.tsx
@@ -2,9 +2,9 @@ const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export async function getTrendingMovies() {
-
-
-  const res = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`);
+  const res = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`, {
+    cache: 'no-store'
+  });
   if (!res.ok) throw new Error("Failed to fetch data");
   const data = await res.json();
   return data.results;
